fb01a06b8357c65812c14e4f08440375a34286b0
wengsummers@gmail.com <wengsummers@gmail.com>
2014-09-14 16:33:52 +0800

    upload assignment first time

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+# There are two functions to create matrixes and cache their
+# inverse matrixes separtately.
 
-## Write a short comment describing this function
+## Function 1: This function is to create a matrix, which is able
+## to store the inverse matrix .
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    Imatrix=NULL
+    setmatrix=function(){x<<-pool;Imatrix<<-NULL}
+    fetchmatrix=function(){x}
+    setImatrix=function(solve){Imatrix<<-solve}
+    fetchImatrix=function(){Imatrix}
+list(setmatrix=setmatrix,fetchmatrix=fetchmatrix
+     ,setImatrix=setImatrix,fetchImatrix=fetchImatrix)
 }
-
-
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+### first cache the inverse of a given matrix.    
+    Imatrix=x$fetchImatrix()
+### If we have had it,return it and tell me it is not a new matrix.
+    if(!is.null(Imatrix)){message("I had it!!")
+    return(Imatrix)}
+### else,get the new matrix,calculate its inverse matrix and
+### stored it.return the inverse matrix newly calcuated.
+    new=x$fetchmatrix()
+    Imatrix=solve(new,...)
+    x$setImatrix(Imatrix)
+    Imatrix
 }
+
